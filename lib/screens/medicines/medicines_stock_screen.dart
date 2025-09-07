d2af1c1650da1ef77ac0cecdf89d7a7ea93ee884
Mohd Inzamam Ahmad <81466387+inzamam9494@users.noreply.github.com>
2025-09-07 16:08:27 +0530

    Refactor color definitions and enhance Medicines Stock Screen UI with search functionality

--- lib/screens/medicines/medicines_stock_screen.dart
+++ lib/screens/medicines/medicines_stock_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healnixd/components/custom_texts.dart';
+import 'package:healnixd/style/color.dart';
+import 'package:healnixd/style/text_style.dart';
 
 class MedicinesStockScreen extends StatelessWidget {
   const MedicinesStockScreen({super.key});
@@ -6,9 +10,85 @@ class MedicinesStockScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Medicines Stock Screen'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.medication, color: Colors.white),
       ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: AppColors.lightBlueBg,
+        title: appBarText(text: "Medicines Stock"),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 12.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: TextField(
+                    cursorColor: Colors.blue,
+                    decoration: InputDecoration(
+                      hintText: "Search Medicines",
+                      hintStyle: AppTextStyles.kCaption12RegularTextStyle
+                          .copyWith(color: Colors.grey),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 18,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.grey.shade50),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      suffixIcon: Container(
+                        padding: EdgeInsets.all(14),
+                        margin: EdgeInsets.all(3),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                        child: Icon(
+                          CupertinoIcons.search,
+                          color: Colors.white,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: IconButton(
+                    color: Colors.blue,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.manage_search,
+                      color: Colors.white,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: AppColors.lightBlueBg,
+      body: Container(child: Column(children: [])),
     );
   }
 }
