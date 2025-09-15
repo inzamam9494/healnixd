990e831fe4febbb49deba711324a7412107da688
Mohd Inzamam Ahmad <81466387+inzamam9494@users.noreply.github.com>
2025-09-15 15:04:25 +0530

    Add Firebase Core integration; update Gradle and Dart files for configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:healnixd/firebase_options.dart';
 import 'package:healnixd/routes/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
